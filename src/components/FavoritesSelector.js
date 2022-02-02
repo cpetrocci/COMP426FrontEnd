e99f56e2b4660f9e61c1c49d4abf6dffa9ecb431
cpetrocci <soccer1italian@hotmail.com>
2022-02-02 15:04:41 -0500

    testing site delpoyment

--- src/components/FavoritesSelector.js
+++ src/components/FavoritesSelector.js
@@ -16,13 +16,13 @@ const FavoritesSelector = ({ currentFavorite, list, updateFavorites, type, theme
         if (type.includes('Film')) {
             list.forEach(character => {if(character.title.toLowerCase().includes(currText)){temp.push(character.title)}});
         } else {
-            list.forEach(character => {if(character.name.toLowerCase().includes(currText)){temp.push(character.name)}});
+            list.forEach(character => {if(character.name.toLowerCase().includes(currText)){temp.push(character.name + "1")}});
         }
         if(temp.length === 0) {
             setMatches(['No Matches']);
             return;
         }
-        setMatches(temp + "1");
+        setMatches(temp);
     }
 
     const updateText = (newText) => {
@@ -37,30 +37,30 @@ const FavoritesSelector = ({ currentFavorite, list, updateFavorites, type, theme
         let found = false;
         let res = '';
 
-        // Films require the ".title" property, the rest use ".name"
-        if (bool) {
-            if (type.includes('Film')) {
-                list.forEach(item => {
-                    if (item.title === text) {
-                        found = true;
-                    }
-                })
-            }
-            else {
-                list.forEach(item => {
-                    if (item.name === text) {
-                        found = true;
-                    }
-                })
-            }
-            // If the text is found in the list, it becomes their favorite and text/matches are reset
-            if (found) {
-                updateFavorites(text);
-                setText('');
-                setMatches([]);
-            }
-            return;
-        }
+        // // Films require the ".title" property, the rest use ".name"
+        // if (bool) {
+        //     if (type.includes('Film')) {
+        //         list.forEach(item => {
+        //             if (item.title === text) {
+        //                 found = true;
+        //             }
+        //         })
+        //     }
+        //     else {
+        //         list.forEach(item => {
+        //             if (item.name === text) {
+        //                 found = true;
+        //             }
+        //         })
+        //     }
+        //     // If the text is found in the list, it becomes their favorite and text/matches are reset
+        //     if (found) {
+        //         updateFavorites(text);
+        //         setText('');
+        //         setMatches([]);
+        //     }
+        //     return;
+        // }
 
         // If the user hits enter
         if (e.keyCode === 13) {
